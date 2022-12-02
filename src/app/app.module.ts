dbf0e163ab6c6909ac05db468257073579afe324
theGeekkit <111545346+theGeekkit@users.noreply.github.com>
2022-12-01 19:05:56 -0600

    widget component 12-1-22

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import {MatCardModule} from '@angular/material/card';
+import { WidgetComponent } from './widget/widget.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent, HeaderComponent, WidgetComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
