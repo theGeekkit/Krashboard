3409c966b30815b13c59cd7b851e4260846ee896
Draike13 <tmbiri91@gmail.com>
2022-12-05 18:07:52 -0600

    fix error lost imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import {MatButtonModule} from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
-import { MatDialogModule} from '@angular/material/dialog';
+import { MatDialogModule } from '@angular/material/dialog';
 import { FormsModule } from '@angular/forms';
-
+import { MatCardModule } from '@angular/material/card';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ModalComponent } from './modal/modal.component';
-
+import { WidgetContainerComponent } from './widget-container/widget-container.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, ModalComponent, ],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    ModalComponent,
+    WidgetContainerComponent,
+  ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
@@ -27,8 +32,8 @@ import { ModalComponent } from './modal/modal.component';
     MatFormFieldModule,
     FormsModule,
     MatInputModule,
-    MatButtonModule
-
+    MatButtonModule,
+    MatCardModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
