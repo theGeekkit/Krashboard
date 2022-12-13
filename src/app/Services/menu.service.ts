7870dc114509c144a69d9f9b46ea13314ee12f50
theGeekkit <111545346+theGeekkit@users.noreply.github.com>
2022-12-12 18:27:48 -0600

    added api input 12-12-22

--- src/app/Services/menu.service.ts
+++ src/app/Services/menu.service.ts
@@ -11,20 +11,24 @@ export class MenuService {
   cards: any = [];
   hamburgerMenu = false;
   widgetTitle: string  = '';
+  widgetContent: string = '';
 
   constructor(public dialog: MatDialog,) {}
 
   openDialog(): void {
     const dialogRef = this.dialog.open(ModalComponent, {
       width: '250px',
-      data: { widgetTitle: this.widgetTitle },
+      data: { widgetTitle: this.widgetTitle,
+      widgetContent: this.widgetContent },
     });
 
     dialogRef.afterClosed().subscribe((result) => {
       console.log('The dialog was closed');
       console.log(this.widgetTitle);
+      console.log(this.widgetContent);
       if (result != null) {
         this.widgetTitle = result;
+        this.widgetContent = result;
       }
     });
   }
